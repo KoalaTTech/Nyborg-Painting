3660c27ec3e940eadd453623d14543aa8b950401
JmanJonz <wjssaan@gmail.com>
2026-04-20 19:39:34 -0600

    basic gutted next.js app

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Nyborg Painting",
+  description: "Professional Painting Services For Salt Lake Area Utah : Salt Lake City SLC, Sandy, Holiday, South Jordan, Provo, Murray, Bountiful, West Jordan, Draper, Cottonwood Heights",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Hub() {
+  return (
+    <main>Yo this is Syborg Painting Hire Me!</main>
+  );
+}
